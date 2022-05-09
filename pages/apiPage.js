871513205bddc661ca3d5e01f39ac29b572b2543
Théo Michel <theo.michel2@outlook.fr>
2022-05-09 15:53:17 +0200

    presentation api avec next

---
+++ pages/apiPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const apiPage = () => {
+  return (
+    <div>
+      <h2>Api des projets</h2>
+      <p>Notice : faire une requête sur /api/projects</p>
+    </div>
+  );
+};
+
+export default apiPage;
